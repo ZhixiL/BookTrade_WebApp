569f82fc5f1facf3c3f2570ec602f53e78b9f3aa
Yuki Zhang <mamodaisukiyuki@gmail.com>
2021-02-15 03:10:26 -0500

    added functions for homepage and login page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from inpforms import signinForm, signupForm
-
+import os
 '''
 --- DATABASE EXPLAIN ---
 The models below will be declared using SQLAlchemy ORM
@@ -13,30 +13,64 @@ More Reference avaliable on above link.
 --- comment by Zhixi Lin ---
 '''
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///webapp.db'#linking with local SQLite3 database named webapp.db
-wadb = SQLAlchemy(app) #Web app database, referencing 
+app = Flask(
+    __name__, static_folder='templates')
+# linking with local SQLite3 database named webapp.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///webapp.db'
+wadb = SQLAlchemy(app)  # Web app database, referencing
 
-class Account(wadb.Model): #This will be a model/table mappping within our wadb(web app database)
-    __tablename__ = 'account' #table name will generally be lower case
-    #When Refer back to this table, use the lower case table name
-    id = wadb.Column(wadb.Integer, primary_key=True) #Other table use this key to refer back to this table.
-    firstname = wadb.Column(wadb.String(30), nullable=False)#(!nullable)name can't be empty
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+
+class Account(wadb.Model):  # This will be a model/table mappping within our wadb(web app database)
+    __tablename__ = 'account'  # table name will generally be lower case
+    # When Refer back to this table, use the lower case table name
+    # Other table use this key to refer back to this table.
+    id = wadb.Column(wadb.Integer, primary_key=True)
+    # (!nullable)name can't be empty
+    firstname = wadb.Column(wadb.String(30), nullable=False)
     lastname = wadb.Column(wadb.String(30), nullable=False)
-    username = wadb.Column(wadb.String(30), nullable=False, unique=True) #unique because every user need their own username to login
-    avatar = wadb.Column(wadb.String(30),default='///templates/images/default_avatar.jpg',nullable=False)
-    password = wadb.Column(wadb.String(15),nullable=False)
+    # unique because every user need their own username to login
+    username = wadb.Column(wadb.String(30), nullable=False, unique=True)
+    avatar = wadb.Column(wadb.String(
+        30), default='///templates/images/default_avatar.jpg', nullable=False)
+    password = wadb.Column(wadb.String(15), nullable=False)
     email = wadb.Column(wadb.String(100), nullable=False, unique=True)
-    fsuid = wadb.Column(wadb.String(10), default='None', nullable=False, unique=True)
-    num_of_posts = wadb.Column(wadb.Integer, default=0, nullable=True) #number of posts by the unique user
+    fsuid = wadb.Column(wadb.String(10), default='None',
+                        nullable=False, unique=True)
+    # number of posts by the unique user
+    num_of_posts = wadb.Column(wadb.Integer, default=0, nullable=True)
 
 
-class Post(wadb.Model): #relation model with the model/table Account to let the user post listing on the site
-    __tablename__ =  'post'
-    id = wadb.Column(wadb.Integer, primary_key=True) 
-    creator = wadb.Column(wadb.Integer, wadb.ForeignKey('account.id')) #this will connect back to the account through account's 
-    header = wadb.Column(wadb.String(30), nullable=False) # User must input a Title of their post
-    body = wadb.Column(wadb.String(100), nullable=True) # User is able to put a body to their post
+class Post(wadb.Model):  # relation model with the model/table Account to let the user post listing on the site
+    __tablename__ = 'post'
+    id = wadb.Column(wadb.Integer, primary_key=True)
+    # this will connect back to the account through account's
+    creator = wadb.Column(wadb.Integer, wadb.ForeignKey('account.id'))
+    # User must input a Title of their post
+    header = wadb.Column(wadb.String(30), nullable=False)
+    # User is able to put a body to their post
+    body = wadb.Column(wadb.String(100), nullable=True)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = signinForm()
+    if form.validate_on_submit():
+        if form.username.data == 'zack' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
