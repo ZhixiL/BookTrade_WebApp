b8a47cc61a8cf1057e6bf9083b69e1bc3ac6ce8c
ZhixiL <zl19@my.fsu.edu>
2021-02-15 00:45:46 -0500

    Updated the Account & Post Models, link them through foreignKey relation, and tested with sqlite3

--- app.py
+++ app.py
@@ -27,17 +27,16 @@ class Account(wadb.Model): #This will be a model/table mappping within our wadb(
     avatar = wadb.Column(wadb.String(30),default='///templates/images/default_avatar.jpg',nullable=False)
     password = wadb.Column(wadb.String(15),nullable=False)
     email = wadb.Column(wadb.String(100), nullable=False, unique=True)
-    num_of_posts = wadb.Column(wadb.Integer, default=0, nullable=True) #number of posts by the unique user,
-    #will add attribute that connects account with post at a later stage - Zhixi Lin
+    fsuid = wadb.Column(wadb.String(10), default='None', nullable=False, unique=True)
+    num_of_posts = wadb.Column(wadb.Integer, default=0, nullable=True) #number of posts by the unique user
 
-class Post(Account, wadb.Model): #derived model from the model/table Account to let the user post listing on the site
+
+class Post(wadb.Model): #relation model with the model/table Account to let the user post listing on the site
     __tablename__ =  'post'
+    id = wadb.Column(wadb.Integer, primary_key=True) 
+    creator = wadb.Column(wadb.Integer, wadb.ForeignKey('account.id')) #this will connect back to the account through account's 
     header = wadb.Column(wadb.String(30), nullable=False) # User must input a Title of their post
     body = wadb.Column(wadb.String(100), nullable=True) # User is able to put a body to their post
-    #need to connect back to the primary_key of account, will be added later - Zhixi Lin
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
