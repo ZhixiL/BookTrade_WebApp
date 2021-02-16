a35fda04129e07e9546f0bd30c65a4e4496df253
ZhixiL <zl19@my.fsu.edu>
2021-02-16 15:51:38 -0500

    updated the comment

--- inpforms.py
+++ inpforms.py
@@ -7,22 +7,21 @@ Followed are the references if needed:
 https://wtforms.readthedocs.io/en/2.3.x/validators/#built-in-validators
 https://flask-wtf.readthedocs.io/en/stable/
 
+DataRequried() checks for that there need to be something entered
+Email() check for correctly formatted email input
+Length(min, max) specify the allowed length of input
+EqualTo() checks if input is equal to given value
 -- comment & code by Zhixi Lin --
 '''
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
-'''
-DataRequried checks for that there need to be something entered
-Email check for correctly formatted email input
-Length(min, max) specify the allowed length of input
-EqualTo() checks if input is equal to given value
-'''
+
 
 class signupForm(FlaskForm):
     firstname = StringField('FirstName', validators=[DataRequired(), Length(min=1,max=30)])
     lastname = StringField('LastName', validators=[DataRequired(), Length(min=1,max=30)])
-    username = StringField('Username', validators=[DataRequired(), Length(min=5,max=30)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2,max=30)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=5,max=15)])
     #Can add password requirement that forces use of num & letter later
     password_again = PasswordField('Enter Password Again', validators=[EqualTo('password')])
