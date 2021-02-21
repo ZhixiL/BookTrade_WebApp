b357a254d6c3c4f08137113dffd53a827a3ea115
ZhixiL <zl19@my.fsu.edu>
2021-02-20 21:44:52 -0500

    added a template for database modification

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 #This Program Sets up the SQLite3 Database
+#Function - Solely for adding info to database for testing purpose.
+# --- by Zhixi Lin ---
 import sqlite3
+from app import wadb, Account, Post
+import datetime
+import time
+wadb.create_all() #create the database.
 
-revdb = sqlite3.connect('webapp.db')
-print ("Opened webapp.db databse successfully!")
-revdb.close()
+#create Account, use the following template & fill it with info you want:
+#acc = Account(lastname = "last", firstname = "first", username = "test", password = "pass", email = "test@my.fsu.edu", fsuid = "testid")
+#wadb.session.add(acc) #add the account into database
+#wadb.session.commit() #commit these added user into the database.
+
+
