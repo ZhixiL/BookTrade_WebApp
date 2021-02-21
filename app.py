5c3b5644e492204ccf149914e0c72342bd3f78e3
ZhixiL <zl19@my.fsu.edu>
2021-02-21 16:16:09 -0500

    Organized Code By Contribution

--- app.py
+++ app.py
@@ -24,31 +24,7 @@ wadb = SQLAlchemy(app)  # Web app database, referencing
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-
-class Account(wadb.Model):  # This will be a model/table mappping within our wadb(web app database)
-    __tablename__ = 'account'  # table name will generally be lower case
-    # When Refer back to this table, use the lower case table name
-    # (!nullable)name can't be empty
-    firstname = wadb.Column(wadb.String(30), nullable=False)
-    lastname = wadb.Column(wadb.String(30), nullable=False)
-    # unique because every user need their own username to login
-    username = wadb.Column(wadb.String(30), nullable=False,
-                           unique=True, primary_key=True)
-    # The username will also served as the primary key for other table to reference back.
-    avatar = wadb.Column(wadb.String(
-        30), default='///templates/images/default_avatar.jpg', nullable=False)
-    password = wadb.Column(wadb.String(15), nullable=False)
-    email = wadb.Column(wadb.String(100), nullable=False, unique=True)
-    fsuid = wadb.Column(wadb.String(10), default='None', nullable=False)
-    # number of posts by the unique user
-    num_of_posts = wadb.Column(wadb.Integer, default=0, nullable=True)
-
-    def __repr__(self):  # Important, for when you cann the object, it returns tuple
-        return 'Account({firstname},{lastname},{username},{avatar},{email},{fsuid})'.format(
-            firstname=self.firstname, lastname=self.lastname, username=self.username,
-            avatar=self.avatar, email=self.email, fsuid=self.fsuid)
-
-
+#Following are the code by Wesley & Zhixi Lin (Zack)
 class Post(wadb.Model):  # relation model with the model/table Account to let the user post listing on the site
     __tablename__ = 'post'
     id = wadb.Column(wadb.Integer, primary_key=True)
@@ -75,27 +51,36 @@ class Post(wadb.Model):  # relation model with the model/table Account to let th
         return 'Account({time},{by},{bookname},{price},{stat},{college},{picture},{description})'.format(
             time=self.time, by=self.by, bookname=self.bookname, price=self.price, stat=self.stat, college=self.college,
             picture=self.picture, description=self.description)
+#End Wesley & Zack
 
+#Following are the code by Zhixi Lin (Zack)
+class Account(wadb.Model):  # This will be a model/table mappping within our wadb(web app database)
+    __tablename__ = 'account'  # table name will generally be lower case
+    # When Refer back to this table, use the lower case table name
+    # (!nullable)name can't be empty
+    firstname = wadb.Column(wadb.String(30), nullable=False)
+    lastname = wadb.Column(wadb.String(30), nullable=False)
+    # unique because every user need their own username to login
+    username = wadb.Column(wadb.String(30), nullable=False,
+                           unique=True, primary_key=True)
+    # The username will also served as the primary key for other table to reference back.
+    avatar = wadb.Column(wadb.String(
+        30), default='///templates/images/default_avatar.jpg', nullable=False)
+    password = wadb.Column(wadb.String(15), nullable=False)
+    email = wadb.Column(wadb.String(100), nullable=False, unique=True)
+    fsuid = wadb.Column(wadb.String(10), default='None', nullable=False)
+    # number of posts by the unique user
+    num_of_posts = wadb.Column(wadb.Integer, default=0, nullable=True)
 
-@app.route("/", methods=['GET'])
-@app.route("/index", methods=['GET'])
-def index():
-    if 'user' in session:  # Ensure the user's full name is send to post.html
-        user = Account.query.filter_by(username=session['user']).first(
-        ).firstname + ' ' + Account.query.filter_by(username=session['user']).first().lastname
-    else:
-        user = 'offline'
-
-    bklist = Post.query.order_by(Post.time).limit(
-        12).all()  # 12 most recently posted books
-    return render_template("index.html", user=user, booktitle="none", bklist=bklist)
-
+    def __repr__(self):  # Important, for when you cann the object, it returns tuple
+        return 'Account({firstname},{lastname},{username},{avatar},{email},{fsuid})'.format(
+            firstname=self.firstname, lastname=self.lastname, username=self.username,
+            avatar=self.avatar, email=self.email, fsuid=self.fsuid)
 
 @app.route("/signout")
 def signout():
     session.pop('user', None)
     return redirect("/")
-
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
@@ -125,26 +110,9 @@ def login():
         else:  # user didn't signed in
             return render_template('login.html')
 
-
-@app.route("/post", methods=['POST', 'GET'])
-def post():
-    if 'user' in session:
-        user = Account.query.filter_by(username=session['user']).first(
-        ).firstname + ' ' + Account.query.filter_by(username=session['user']).first().lastname
-    else:
-        user = 'offline'
-    name_of_book = request.form.get('Book Name')
-    post_price = request.form.get('Price')
-    status = request.form.get('Status')
-    category = request.form.get('Category')
-    picture = request.form.get('file')
-    return render_template('post.html', user=user)
-
-
 @app.route('/msg', methods=['POST', 'GET'])
 def msg():
     return render_template("message.html", msg="placeholder")
-
 
 @app.route('/booklist', methods=['GET', 'POST'])
 def booklist():
@@ -159,7 +127,7 @@ def booklist():
         bklist = Post.query.order_by(Post.time).all()
         return render_template("booklist.html", user=user, booktitle="none", bklist=bklist)
 
-    else:  # this is POST request, from search.
+    else:  #This is the case for search
         key = str(request.form['keywords'])
         bklist = Post.query.filter(
             Post.bookname.contains(key)).order_by(Post.time).all()
@@ -167,12 +135,46 @@ def booklist():
             return redirect(url_for('index'))
         else:
             return render_template("booklist.html", user=user, booktitle="none", bklist=bklist)
+#End Zack
 
 
+#following are the code by Hanyan Zhang (Yuki), Zhixi Lin (Zack)
+@app.route("/", methods=['GET'])
+@app.route("/index", methods=['GET'])
+def index():
+    if 'user' in session:  # Ensure the user's full name is send to post.html
+        user = Account.query.filter_by(username=session['user']).first(
+        ).firstname + ' ' + Account.query.filter_by(username=session['user']).first().lastname
+    else:
+        user = 'offline'
+
+    bklist = Post.query.order_by(Post.time).limit(
+        12).all()  # 12 most recently posted books
+    return render_template("index.html", user=user, booktitle="none", bklist=bklist)
+#End Yuki & Zack
+
+
+#Following are the code by Yuanyuan Bao, Wesley White
+@app.route("/post", methods=['POST', 'GET'])
+def post():
+    if 'user' in session:
+        user = Account.query.filter_by(username=session['user']).first(
+        ).firstname + ' ' + Account.query.filter_by(username=session['user']).first().lastname
+    else:
+        user = 'offline'
+    name_of_book = request.form.get('Book Name')
+    post_price = request.form.get('Price')
+    status = request.form.get('Status')
+    category = request.form.get('Category')
+    picture = request.form.get('file')
+    return render_template('post.html', user=user)
+#End Yuanyuan, Wesley
+
+
+#Following are the code by Dennis Majanos, Wesley White
 @app.route('/createAccPage')
 def createAccPage():
     return render_template("createAccount.html")
-
 
 @app.route('/createAcc', methods=['POST', 'GET'])
 def createAcc():
@@ -189,7 +191,7 @@ def createAcc():
             i = 1  # dummy variable
         finally:
             return redirect("/")
-
+#End Dennis, Wesley
 
 if __name__ == '__main__':
     app.run(debug=True)
