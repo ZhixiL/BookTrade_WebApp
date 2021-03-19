6ee4a7a5bf47213c3dbee0bcb74c764a6b186c8f
Yuki Zhang <mamodaisukiyuki@gmail.com>
2021-03-19 14:50:40 -0400

    Added the Account class to Angular

---
+++ fsu-book-trade/src/app/Account.ts
@@ -0,0 +1,25 @@
+export class Account
+{
+    avatar:string;
+    email:string;
+    firstname:string;
+    fsuid:string;
+    lastname:string;
+    num_of_posts:number;
+    password:string;
+    username:number;
+
+
+    constructor(avatar, email, firstname, fsuid, lastname, num_of_posts, password, username)
+    {
+        this.avatar = avatar;
+        this.email = email;
+        this.firstname = firstname;
+        this.fsuid = fsuid;
+        this.lastname = lastname;
+        this.num_of_posts = num_of_posts;
+        this.password = password;
+        this.username = username;
+    }
+
+}
